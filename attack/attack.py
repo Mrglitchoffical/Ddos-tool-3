7a652f23533ce7b2f8638c75b44791f9ad844277
MrYuvraj <162595849+Mrglitchoffical@users.noreply.github.com>
2025-11-10 10:43:55 +0530

    Remove duplicate user agent string
    Removed duplicate user agent string from the list.

--- attack/attack.py
+++ attack/attack.py
@@ -21,7 +21,6 @@ user_agents = [
     "Mozilla/5.0 (X11; Linux x86_64)",
     "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X)",
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, l>
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Fir>
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_0)",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_0) AppleWebKit/605.1.15 (KHT>
